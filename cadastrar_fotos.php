51bce8beb8776662cff32a1fab85fee34b65e515
feliipenevesnow <feliipenevesnow@gmail.com>
2023-11-08 16:22:30 -0300

    Saodnm

--- cadastrar_fotos.php
+++ cadastrar_fotos.php
@@ -1,10 +1,50 @@
 <?php
 include 'fragmentos/header.php';
+
+if(isset($_POST['salvar'])){
+
+    echo "oiiiiiiiiiiiiiiiii";
+
+    $titulo = $_POST['titulo'];
+    $preco = $_POST['preco'];
+
+    $preco = str_replace('.', '', $preco);
+    $preco = str_replace(',', '.', $preco);
+
+    $usuario = $_SESSION['usuario']['codigo'];
+    $imagem;
+    $status = 1;
+
+    print_r($_FILES['foto_perfil']);
+
+    if (isset($_FILES['foto_perfil']) && $_FILES['foto_perfil']['error'] === UPLOAD_ERR_OK) {
+        $uploadDirectory = 'upload/';
+    
+        $extensao = pathinfo($_FILES['foto_perfil']['name'], PATHINFO_EXTENSION);
+    
+        $imagem = time() . '.' . $extensao;
+    
+        $diretorio = $uploadDirectory . time() . '.' . $extensao;
+
+        print_r($imagem_nome);
+    
+        if (move_uploaded_file($_FILES['foto_perfil']['tmp_name'], $diretorio)) {
+    
+           $sql = "insert into foto(usuario, imagem, status, titulo, preco)".
+           " values('$usuario', '$imagem', '$status', '$titulo', '$preco') "; 
+
+    
+            executarInsert($con, $sql);
+        } 
+    
+    } 
+}
+
 ?>
 
 <main>
 
-    <form class="row g-3">
+    <form action="cadastrar_fotos.php" method="POST"  enctype='multipart/form-data'  class="row g-3">
 
         <div class="col-md-6">
             <label for="inputEmail4" class="form-label">Titulo</label>
@@ -12,24 +52,40 @@ include 'fragmentos/header.php';
         </div>
         <div class="col-md-6">
             <label for="inputPassword4" class="form-label">Preço</label>
-            <input class="form-control" name="preco" type="text" 
-            id="valor" data-max-digits="15" maxlength="20" />
+            <input class="form-control" name="preco" type="text" id="valor" data-max-digits="15" maxlength="20" />
         </div>
 
-        <div class="row">
+        <div class="row mt-3">
             <div class="col-md-6 form-group">
                 <label for="foto_perfil">Foto</label>
-                <input type="file" name="foto_perfil" id="foto_perfil" 
-                accept="image/*">
+                <input type="file" name="foto_perfil" id="foto_perfil" accept="image/*">
             </div>
             <div class="col-md-6 form-group">
-                <img id="preview" src="" alt="Preview da foto" 
-                style="max-width: 100px; max-height: 100px;">
+                <img id="preview" src="" alt="Preview da foto"
+                    style="max-width: 100px; max-height: 100px;">
             </div>
         </div>
 
+        <script>
+            document.getElementById('foto_perfil').addEventListener('change', function () {
+                var preview = document.getElementById('preview');
+                var file = document.getElementById('foto_perfil').files[0];
+                var reader = new FileReader();
+
+                reader.onloadend = function () {
+                    preview.src = reader.result;
+                }
+
+                if (file) {
+                    reader.readAsDataURL(file);
+                } else {
+                    preview.src = "";
+                }
+            });
+        </script>
+
         <div class="col-12">
-            <button type="submit" class="btn btn-dark">Cadastrar</button>
+            <button type="submit" class="btn btn-dark" name="salvar">Cadastrar</button>
         </div>
 
     </form>
