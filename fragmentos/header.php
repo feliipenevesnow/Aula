8eb376c07d3ac25aabbbfeeb2fc9327ab932e37d
feliipenevesnow <feliipenevesnow@gmail.com>
2023-11-22 14:31:25 -0300

    rtste

--- fragmentos/header.php
+++ fragmentos/header.php
@@ -1,7 +1,7 @@
 <?php
 if (!isset($_SESSION)) {
     session_start();
-  }
+}
 ?>
 
 <html lang="en">
@@ -12,9 +12,13 @@ if (!isset($_SESSION)) {
     <title>Aula</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
+
 </head>
 
 <body>
@@ -57,7 +61,7 @@ if (!isset($_SESSION)) {
                             <?= $_SESSION['usuario']['nome'] ?>
                         </a>
                     </div>
-                    
+
                     <?php
                     }
                     ?>
